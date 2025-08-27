6261f5a9bfe5d6de5d9007e305b42666ef4c9d69
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-08-27 13:41:05 -0400

    sum to 100

--- week1/main.java
+++ week1/main.java
@@ -1,5 +1,10 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
