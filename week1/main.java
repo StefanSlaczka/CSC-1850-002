c8ef162dbeb4b4ba92c10b7b61f1e422d5926ae1
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-08-27 14:00:48 -0400

    prime number to 1000

--- week1/main.java
+++ week1/main.java
@@ -1,12 +1,9 @@
 public class main {
     public static void main(String[] args) {
-        int sum = 0;
-        int i = 1;
-        do {
-            System.out.println(i);
-            sum += i;
-            i++;
-        } while( i <= 100);
-        System.out.println(sum);
+        for (int i = 2; i <= 1000; i++){
+            if (i%2 == 1){
+                System.out.println(i);
+            }
+        }
     }
 }
