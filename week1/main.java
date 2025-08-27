7553b2f43015a517508b3d517d276d3868dfe279
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-08-27 14:04:33 -0400

    real prime

--- week1/main.java
+++ week1/main.java
@@ -1,9 +1,14 @@
 public class main {
     public static void main(String[] args) {
         for (int i = 2; i <= 1000; i++){
-            if (i%2 == 1){
+            if (prime(i)){
                 System.out.println(i);
             }
         }
     }
+    public static Boolean prime (int i){
+        for (int d = 2; d < i; d ++){
+            if (i%d == 0) return false;
+        } return true;
+    } 
 }
