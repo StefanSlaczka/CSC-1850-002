f5b402ab54d24e45e46c377404398ad42ec64d2a
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-08-27 13:44:31 -0400

    change to do while loop

--- week1/main.java
+++ week1/main.java
@@ -2,11 +2,11 @@ public class main {
     public static void main(String[] args) {
         int sum = 0;
         int i = 1;
-        while (i <= 100) {
+        do {
             System.out.println(i);
             sum += i;
             i++;
-        }
+        } while( i <= 100);
         System.out.println(sum);
     }
 }
