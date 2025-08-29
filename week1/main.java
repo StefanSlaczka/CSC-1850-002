74521855e17afcc5addcaad15a316bbdb55f249e
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-08-29 14:04:22 -0400

    didn't need else

--- week1/main.java
+++ week1/main.java
@@ -16,8 +16,6 @@ public class main {
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
             return true;
         }
-        else {
-            return false;
-        }
+        return false;
     }
 }
