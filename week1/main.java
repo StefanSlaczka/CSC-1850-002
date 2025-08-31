3d6fb650c72a27ed78ac2a9abbbcef89ca53b25a
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-08-31 11:01:58 -0400

    FizzBuzz

--- week1/main.java
+++ week1/main.java
@@ -1,21 +1,22 @@
-import java.util.Random;
 import java.util.Scanner;
 
-public class main {
-	public static void main(String [] args) {
+public class main{
+	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		Random random = new Random();
-		int randomNum = random.nextInt(100) + 1;
-		
-		int guess = 0;
-		
-		while (guess != randomNum) {
-			System.out.println("Please guess a number between 1 to 100");
-			guess = scanner.nextInt();
-			
-			if (guess > randomNum){
-				System.out.println("Your number is too high.");
-			} else { System.out.println("Your number is too low");}
-		}System.out.println("You Won");
+		System.out.println("Enter the amount of number you want to see FizzBuzz");
+		int number = scanner.nextInt();
+		FizzBuzz(number);
+	}
+	public static void FizzBuzz(int number){
+		for (int i = 0; i <= number; i++){
+			if (i % 15 == 0){
+				System.out.println("FizzBuzz");
+			} else if (i % 5 == 0){
+				System.out.println("Buzz");
+			}else if (i % 3 == 0){
+				System.out.println("Fizz");
+
+			}else { System.out.println(i);}
+		}
 	}
 }
