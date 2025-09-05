39b4e15ab65e40c7c0512ef3d7e7954ecbbb81aa
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-09-05 14:13:12 -0400

    classes

--- week1/main.java
+++ week1/main.java
@@ -1,39 +1,17 @@
-class Point {
-	double x;
-	double y;
+class Employee {
 
-	public void move(double x, double y){
-		this.x = x; this.y = y;
+	int employeeNumber;
+	String firstName;
+	String lastName;
+	String position;
+	String address;
+
+	public void changePosition(String position){
+		//position = newPosition;
 	}
-
-	public int move(char dir, double dist){
-		if(dir == 'N' || dir == 'n'){
-			y+=dist;
-		}
-		else if(dir == 'S' || dir == 's'){
-			y-=dist;
-		}
-		else if(dir == 'E' || dir == 'e'){
-			x+=dist;
-		}
-		else if (dir == 'W' || dir == 'w'){
-			x-=dist;
-		}
-		else{
-			return -1;
-		}
-		return 0;
-	}
-
-	/* Constructor
-	Point(double x, double y){
-
-	}*/
 }
-
 public class main{
 	public static void main(String[] arr){
-		Point p1 = new Point();
-		System.err.println(p1.x);
+
 	}
 }
