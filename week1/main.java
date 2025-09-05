01a3df3afd5aefa093120ac08d40faefccb9936e
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-09-05 09:11:43 -0400

    finshed the code

--- week1/main.java
+++ week1/main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+// make a bubble sort
+
 public class main{
 	public static void main(String[] args){		// insulise it
 		// int[] arr;
@@ -17,6 +19,7 @@ public class main{
 
 		recieveInput(list, size);
 		double avg = findAvg(list, size);
+		System.err.println(avg);
 	}
 
 	public static void recieveInput(int [] list, int size) {
@@ -29,5 +32,10 @@ public class main{
 	public static double findAvg(int [] list, int size) {
 		double avg = 0;
 		// finsh code
+		for (int j=0; j < size; j ++ ) {
+			avg += list[j];
+		}
+		avg /= size;
+		return avg;
 	}
 }
