890ff513d2294c6ef05972fb0616f1a668738960
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-10-12 12:32:31 -0400

    home work done

---
+++ homeWork6/Test.java
@@ -0,0 +1,26 @@
+class Test{
+	public static void main(String[] args){
+		Fan f1 = new Fan();
+		f1.setSpeed(Fan.FAST);
+		f1.setRadius(10);
+		f1.setColor("yellow");
+		f1.setOn(true);
+
+		System.out.println("Fan 1");
+		System.out.println(f1.toString());
+
+		Fan f2 = new Fan();
+		f2.setSpeed(Fan.MEDIUM);
+		f2.setRadius(6.7);
+		f2.setColor("green");
+		f2.setOn(false);
+
+		System.out.println("Fan 2");
+		System.out.println(f2.toString());
+
+
+		System.out.println("Direct print");
+		System.out.println("fan 1"+ f1);
+		System.out.println("fan 2" + f2);
+	}
+}
