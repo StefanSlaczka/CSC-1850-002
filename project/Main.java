c984946e671f0e9376b8378784114f4d3c776a99
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-11-07 13:34:54 -0500

    Project

---
+++ project/Main.java
@@ -0,0 +1,30 @@
+/*
+    first make a grid
+        ???
+        ???
+        ???
+
+    use input to change the board
+        with row and colum
+            row 0 - 2
+            column 0 -2
+
+    how to make this
+
+    make grid
+    String draw;
+
+    do {
+
+        inputs if spot is not taken loop back if it is
+        check if winner
+
+        check if no-one won after 4 loops result = draw
+
+        computer input if spot is not taken loop back if it is
+        check if loser
+    
+    } while(!game)
+    
+
+*/
