58b63e3d5b322bea486fccca0e3818ed55e5da50
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-12-01 15:09:18 -0500

    Project

--- project/Main.java
+++ project/Main.java
@@ -1,6 +1,34 @@
+import javax.swing.*;
+
 class Main {
     public static void main(String[] args) {
-        
-    }
+        JFrame frame = new JFrame("TikTacToe");
+        frame.setSize(500, 600);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JButton[] buttons = new JButton[9];
+
+        int xStart = 100;
+        int yStart = 100;
+        int size = 100;
+        int index = 0;
+
+        // Creates 3*3 grid of buttons
+
+        for(int row = 0; row < 3; row++){
+            for (int col = 0; col < 3; col++){
+                buttons[index] = new JButton("Pick Me");
+                buttons[index].setBounds(
+                    xStart + col * size,
+                    yStart + row * size,
+                    size, size
+                );
+                frame.add(buttons[index]);
+                index++;
+            }
+        }
+        
+        frame.setVisible(true);
+    }
 }
